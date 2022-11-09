99e17cdf819d38c3af7ce4a242536c025fc64fca
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-09 17:50:52 +0100

    Update run.sh
    Activate Erlang during startup.

--- kazoo/build/run.sh
+++ kazoo/build/run.sh
@@ -10,6 +10,8 @@ export KZname="-name $KAZOO_NODE"
 
 [ ! -e kazoo/erlang.mk ] && echo "Container is built without Kazoo sources, please specify with KAZOO_SOURCE env variable" && exit 1
 
+. /home/user/erlang/activate
+
 cd kazoo
 if [ -e ./skip_build ]
 then
