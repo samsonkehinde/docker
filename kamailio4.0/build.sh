c170ae78bf4268dcb7a064d7eb73df6b0904d4b8
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-22 18:22:57 +0100

    Delete kamailio4.0 directory

--- kamailio4.0/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-NETWORK=${NETWORK:-"kazoo"}
-REPO=${2:-"https://github.com/kamailio/kamailio.git"}
-COMMIT=${1:-"$(../bin/get-commit $REPO)"}
-echo $COMMIT > etc/commit
-docker build $BUILD_FLAGS --build-arg REPO=$REPO -t $NETWORK/kamailio .
--- kamailio4.0/build/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-COMMIT=$(cat /root/commit)
-git clone https://github.com/mreiferson/libevbuffsock libevbuffsock
-git clone https://github.com/nsqio/libnsq libnsq
-git clone $REPO kamailio
-cd kamailio
-git checkout $COMMIT
-cd ../
