b4fa00b14eae61a56087a78d34bb722e4ed4e353
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-22 18:22:23 +0100

    Update run.sh

--- couchdb/run.sh
+++ couchdb/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 FLAGS=${1:-"-td"}
 IMAGE=${2:-"kazoo/couchdb"}
-NETWORK=${NETWORK:-"kazoo"}
+NETWORK=${NETWORK:-"host"}
 NAME=couchdb.$NETWORK
 
 if [ -n "$(docker ps -aq -f name=$NAME)" ]
