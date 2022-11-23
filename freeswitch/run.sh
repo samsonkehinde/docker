726ded4fd76338b4b8a0ff79c74aeca6c750a1c3
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-23 08:59:17 +0100

    Update run.sh

--- freeswitch/run.sh
+++ freeswitch/run.sh
@@ -2,8 +2,8 @@
 FLAGS=${1:-"-td"}
 IMAGE=${2:-"kazoo/freeswitch"}
 NETWORK=${NETWORK:-"host"}
-NAME=${NAME:-"freeswitch.$NETWORK"}
-KAMAILIO=${KAMAILIO:-"kamailio.$NETWORK"}
+NAME=${NAME:-"freeswitch.kazoo"}
+KAMAILIO=${KAMAILIO:-"kamailio.kazoo"}
 
 if [ -n "$(docker ps -aq -f name=$NAME)" ]
 then
@@ -14,7 +14,7 @@ then
 fi
 
 echo -n "starting: $NAME "
-docker run $FLAGS --net $NETWORK -h $NAME --name $NAME --env RABBITMQ=${RABBITMQ:-"rabbitmq.$NETWORK"} $IMAGE
+docker run $FLAGS --net $NETWORK -h $NAME --name $NAME --env RABBITMQ=${RABBITMQ:-"rabbitmq.kazoo"} $IMAGE
 
 echo -n "adding dispatcher $NAME to kamailio $KAMAILIO "
 docker exec $KAMAILIO dispatcher_add.sh 1 $NAME
