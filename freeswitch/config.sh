e66197b1787761f2005ffe3f679bd83050b28da4
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-23 16:12:40 +0100

    Create config.sh

---
+++ freeswitch/config.sh
@@ -0,0 +1,8 @@
+# FreeSWITCH Settings
+switch_branch=stable                        # master, stable
+switch_source=true                          # true (source compile) or false (binary package)
+switch_package=false                        # true (binary package) or false (source compile)
+switch_version=1.10.8                       # which source code to download, only for source
+switch_tls=true                             # true or false
+switch_token=                               # Get the auth token from https://signalwire.com
+                                            # Signup or Login -> Profile -> Personal Auth Token
