f3ae3f6a2fea329cca8e7e2a3a1b7b1dd2de7ff3
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-24 12:28:19 +0100

    Update build.sh

--- kazoo/build.sh
+++ kazoo/build.sh
@@ -4,17 +4,10 @@ COMMIT=${1:-"$(../bin/get-commit $REPO)"}
 
 BRANCH=${BRANCH:-""}
 SKIP_BUILD=${SKIP_BUILD:-""}
-NETWORK=${NETWORK:-"kazoo"}
 
 UID=$(id -u)
 GID=$(id -g)
 
 echo Using repository:$REPO commit:$COMMIT branch:$BRANCH
 echo $COMMIT > etc/commit
-docker build $BUILD_FLAGS -t $NETWORK/kazoo \
-	--build-arg REPO=$REPO \
-	--build-arg SKIP_BUILD=$SKIP_BUILD \
-	--build-arg UID=$UID \
-	--build-arg GID=$GID \
-	--build-arg BRANCH=$BRANCH \
-	.
+docker build $BUILD_FLAGS -t kazoo/kazoo --build-arg REPO=$REPO --build-arg SKIP_BUILD=$SKIP_BUILD --build-arg UID=$UID --build-arg GID=$GID --build-arg BRANCH=$BRANCH .
