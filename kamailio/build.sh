120d00b08110753120f8f02b71bda9fd3bddbbe1
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-24 12:29:54 +0100

    Update build.sh

--- kamailio/build.sh
+++ kamailio/build.sh
@@ -1,12 +1,7 @@
 #!/bin/sh -e
-NETWORK=${NETWORK:-"kazoo"}
 REPO=${2:-"https://github.com/kamailio/kamailio.git"}
 COMMIT=${1:-"$(../bin/get-commit $REPO)"}
 
 echo $COMMIT > etc/commit
 
-docker build $BUILD_FLAGS \
-	--build-arg REPO=$REPO \
-	--build-arg CONFIG=ef98bf453cb81475bbf5ccbc0635749a975b093c \
-	-t $NETWORK/kamailio \
-	.
+docker build $BUILD_FLAGS --build-arg REPO=$REPO --build-arg CONFIG=ef98bf453cb81475bbf5ccbc0635749a975b093c -t kazoo/kamailio .
