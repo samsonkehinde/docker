83fc5c2ff2344360995c333a05e4676489e04622
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-24 12:31:24 +0100

    Update build.sh

--- monster-ui/build.sh
+++ monster-ui/build.sh
@@ -1,12 +1,9 @@
 #!/bin/sh -e
-NETWORK=${NETWORK:-"kazoo"}
+
 REPO=${2:-"https://github.com/2600hz/monster-ui.git"}
 COMMIT=${1:-"$(../bin/get-commit $REPO)"}
+
 echo Using repository $REPO commit $COMMIT
 echo $COMMIT > etc/commit
 [ -e ./apps ] && APPS=$(cat apps)
-docker build $BUILD_FLAGS \
-	-t $NETWORK/monster-ui \
-	--build-arg APPS="$APPS" \
-	--build-arg TOKEN=$TOKEN \
-	--build-arg REPO=$REPO .
+docker build $BUILD_FLAGS -t kazoo/monster-ui --build-arg APPS="$APPS" --build-arg TOKEN=$TOKEN --build-arg REPO=$REPO .
