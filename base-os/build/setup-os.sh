c821f975f4ccd364f5c4972008c8ca183c5f3615
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-24 14:10:46 +0100

    Update setup-os.sh

--- base-os/build/setup-os.sh
+++ base-os/build/setup-os.sh
@@ -4,9 +4,7 @@ useradd -s /bin/bash -m user
 apt-get -y update
 apt-get -y upgrade
 
-apt-get install -y --force-yes vim curl wget git \
-	build-essential \
-	unixodbc-dev libssh2-1-dev libssl-dev libncurses5-dev
+apt-get install -y --force-yes vim curl wget git build-essential unixodbc-dev libssh2-1-dev libssl-dev libncurses5-dev
 
 wget -O - http://files.freeswitch.org/repo/deb/debian/key.gpg | apt-key add - 
 
