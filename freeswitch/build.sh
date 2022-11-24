0f71a63a37340e08cea3ea45043e5f36907e0ecb
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-24 12:31:58 +0100

    Update build.sh

--- freeswitch/build.sh
+++ freeswitch/build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -e
-NETWORK=${NETWORK:-"kazoo"}
 REPO=${2:-"https://github.com/signalwire/freeswitch.git"}
 # Grab the most recent commit from this repository if no particular commit is requested.
 COMMIT=${1:-"$(../bin/get-commit $REPO)"}
 
 echo $COMMIT > etc/commit
 
-docker build $BUILD_FLAGS --build-arg REPO=$REPO -t $NETWORK/freeswitch .
+docker build $BUILD_FLAGS --build-arg REPO=$REPO -t kazoo/freeswitch .
