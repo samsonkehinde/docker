fb22e79a4ef1c3088d10469ebd6ca97f6bf78f7b
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-24 14:11:11 +0100

    Update setup-os.sh

--- kamailio/build/setup-os.sh
+++ kamailio/build/setup-os.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 apt-get update
-apt-get -y install make flex bison libmysqlclient-dev libssl-dev libcurl4-openssl-dev libxml2-dev libpcre3-dev librabbitmq-dev \
+apt-get -y --force-yes install make flex bison libmysqlclient-dev libssl-dev libcurl4-openssl-dev libxml2-dev libpcre3-dev librabbitmq-dev \
 	libjson-c-dev pkg-config  libevent-dev libev-dev uuid-dev libunistring-dev xsltproc python libsnmp-dev bison
 apt-get -y clean
