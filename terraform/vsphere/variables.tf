8e136fd4a370a275597968e5823342768ecf5ae8
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-25 10:12:24 +0100

    Update variables.tf

--- terraform/vsphere/variables.tf
+++ terraform/vsphere/variables.tf
@@ -2,9 +2,9 @@ variable "vsphere_server" {}
 variable "vsphere_user" {}
 variable "vsphere_password" {}
 variable "vm-id"{
-  default: "2"
+  default = "2"
 }
 variable "vm-name"{
-  default: "racher"
+  default = "racher"
 }
 
