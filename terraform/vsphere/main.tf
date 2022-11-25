cf5de0a830c3d28b3cd9dde8b4b79990efa74e04
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-25 09:30:52 +0100

    Create main.tf

---
+++ terraform/vsphere/main.tf
@@ -0,0 +1,47 @@
+## Configure the vSphere Provider
+provider "vsphere" {
+    vsphere_server = "${var.vsphere_server}"
+    user = "${var.vsphere_user}"
+    password = "${var.vsphere_password}"
+    allow_unverified_ssl = true
+}
+
+## Build VM
+data "vsphere_datacenter" "dc" {
+  name = "ha-datacenter"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "datastore1"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_resource_pool" "pool" {}
+
+data "vsphere_network" "mgmt_lan" {
+  name          = "LAN1"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_virtual_machine" "test2" {
+  name             = "${var.vm-id}"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+
+  num_cpus   = 4
+  memory     = 8192
+  wait_for_guest_net_timeout = 0
+  guest_id = "debain8_64Guest"
+  nested_hv_enabled =true
+  network_interface {
+   network_id     = "${data.vsphere_network.mgmt_lan.id}"
+   adapter_type   = "vmxnet3"
+  }
+
+  disk {
+   size             = 16
+   name             = ""${var.disk-name}""
+   eagerly_scrub    = false
+   thin_provisioned = true
+  }
+}
