5ac875042d2bb88326202736e8708146b0d022ff
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-26 20:37:57 +0100

    Delete kamailio4.0 directory

--- kamailio4.0/build/setup-os.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-apt-get -y install \
-	make flex bison libmysqlclient-dev \
-	libssl-dev libcurl4-openssl-dev \
-	libxml2-dev libpcre3-dev librabbitmq-dev \
-	libjson-c-dev pkg-config  libevent-dev libev-dev \
-	uuid-dev libunistring-dev xsltproc \
-	python
-apt-get -y clean
