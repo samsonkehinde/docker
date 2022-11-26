6630ab122f718cb7461337f6ee05749949ac748b
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-26 14:17:09 +0100

    Update setup-os.sh

--- kazoo-erlang/build/setup-os.sh
+++ kazoo-erlang/build/setup-os.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
-apt-get install -y procps make python build-essential zip unzip libexpat-dev unixodbc-dev libssh2-1-dev libssl-dev libncurses5-dev autoconf
+apt-get install -y --force-yes procps make python build-essential zip unzip libexpat-dev unixodbc-dev libssh2-1-dev libssl-dev libncurses5-dev autoconf
 
 apt-get -y clean
