1245d306e29f6cad2fe8bf301af3260bd06de79d
Samson Kehinde <kehindesamson@gmail.com>
2022-11-27 17:06:49 +0000

    Update zone.sh file

--- dual-hosts/zone.sh
+++ dual-hosts/zone.sh
@@ -3,8 +3,8 @@
 read -p "Enter the IP of your RABBITMQ server: " RABBITMQ
 read -p "Enter the IP of your COUCHDB server: " COUCHDB
 
-export $RABBITMQ
-export $COUCHDB
+export RABBITMQ=$RABBITMQ
+export COUCHDB=$COUCHDB
 
 kazoo/run.sh
 freeswitch/run.sh
