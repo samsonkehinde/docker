8791844fe3bda3d6a0e375230b29c86c65f6987f
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-28 13:48:46 +0100

    Update run.sh

--- rabbitmq/run.sh
+++ rabbitmq/run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -e
 FLAGS=${1:-"-td"}
-NAME=${NAME:-"rabbitmq"}
+NAME=${NAME:-"rabbitmq.kazoo"}
 
-if [ -n "$(docker ps -aq -f name=$NAME)" ]
-then
-	echo -n "stopping: "
-	docker stop -t 1 $NAME
-	echo -n "removing: "
-	docker rm -f $NAME
-fi
+docker ps -aq -f name=$NAME | while read ID; do
+        echo -n "stopping: "
+        docker stop -t 1 $ID
+        echo -n "removing: "
+        docker rm -f $ID
+done
+
 echo -n "starting: $NAME "
 docker run $FLAGS --net host --name $NAME kazoo/rabbitmq
