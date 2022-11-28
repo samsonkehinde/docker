9399c022eeebcadb3dbfe98c22564bc8f9f3c057
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-28 13:37:17 +0100

    Update run.sh

--- freeswitch/run.sh
+++ freeswitch/run.sh
@@ -9,7 +9,7 @@ while read ID; do
 	docker stop -t 1 ID
 	echo -n "removing: "
 	docker rm -f ID
-done < docker ps -aq -f name=$NAME
+done < (docker ps -aq -f name=$NAME)
 
 echo -n "starting: $NAME "
 docker run $FLAGS --net host --name $NAME -e RABBITMQ=$RABBITMQ $IMAGE
