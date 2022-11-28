e6ed7c6b6a6210b6ea667d0c12ec694b960c59d0
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-28 13:51:17 +0100

    Update run.sh

--- kamailio/run.sh
+++ kamailio/run.sh
@@ -1,17 +1,17 @@
 #!/bin/sh -e
 FLAGS=${1:-"-td"}
-NAME=${NAME:-"kamailio"}
+NAME=${NAME:-"kamailio.kazoo"}
 
 # Request for the IP of RabbitMQ to use for this Kamailio Instance.
 read -p "Enter RabbitMQ IP:" RABBITMQ
 
-if [ -n "$(docker ps -aq -f name=$NAME)" ]
-then
-   echo -n "stopping: "
-   docker stop -t 1 $NAME
-   echo -n "removing: "
-   docker rm -f $NAME
-fi
+docker ps -aq -f name=$NAME | while read ID; do
+        echo -n "stopping: "
+        docker stop -t 1 $ID
+        echo -n "removing: "
+        docker rm -f $ID
+done
+
 echo -n "starting: $NAME "
 
 #docker run $FLAGS --net host --name $NAME --add-host=rabbitmq-server:$RABBITMQ kazoo/kamailio
