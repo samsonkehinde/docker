8079426e8b3f9f490b6bebaf274e5641b8aafa9c
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-28 13:50:50 +0100

    Update run.sh

--- monster-ui/run.sh
+++ monster-ui/run.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -e
 FLAGS=${1:-"-td"}
-NAME=${NAME:-"monster-ui"}
+NAME=${NAME:-"monster-ui.kazoo"}
 
 read -p "Enter Kazoo Server IP:" KAZOO
 
-if [ -n "$(docker ps -aq -f name=$NAME)" ]
-then
-   echo -n "stopping: "
-   docker stop -t 1 $NAME
-   echo -n "removing: "
-   docker rm -f $NAME
-fi
+docker ps -aq -f name=$NAME | while read ID; do
+        echo -n "stopping: "
+        docker stop -t 1 $ID
+        echo -n "removing: "
+        docker rm -f $ID
+done
+
 echo -n "starting: $NAME "
 
 docker run $FLAGS --net host --name $NAME kazoo/monster-ui --add-host kazoo-server=$KAZOO
