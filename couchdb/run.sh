54ae47f3bef21c95b0e80b06275e15b9dc50c3f1
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-28 13:49:59 +0100

    Update run.sh

--- couchdb/run.sh
+++ couchdb/run.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -e
 FLAGS=${1:-"-td"}
 IMAGE=${2:-"kazoo/couchdb"}
-NAME=${NAME:-"couchdb"}
+NAME=${NAME:-"couchdb.kazoo"}
 
-if [ -n "$(docker ps -aq -f name=$NAME)" ]
-then
-   echo -n "stopping: "
-   docker stop -t 1 $NAME
-   echo -n "removing: "
-   docker rm -f $NAME
-fi
+docker ps -aq -f name=$NAME | while read ID; do
+        echo -n "stopping: "
+        docker stop -t 1 $ID
+        echo -n "removing: "
+        docker rm -f $ID
+done
+
 echo -n "starting: $NAME "
 
 docker run $FLAGS --net host --name $NAME $IMAGE
