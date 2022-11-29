89d0f448df9294b7a3c8e9a4a5520f9a88466c3c
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-29 17:50:28 +0100

    Update kazoo.sh

--- dual-hosts/kazoo.sh
+++ dual-hosts/kazoo.sh
@@ -4,3 +4,12 @@
 ../couchdb/run.sh
 ../kamailio/run.sh
 ../monster-ui/run.sh
+
+for CONTAINER in monster-ui rabbitmq kamailio couchdb
+do
+	if [ "$(docker inspect -f {{.State.Running}} $CONTAINER.$NETWORK)" = "true" ]
+	then
+		echo '127.0.0.1\n'
+	fi
+done
+exit
