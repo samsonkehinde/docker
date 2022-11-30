a60d02d71288a0a7ff3b5e715e63bd2cbfb773f7
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-30 10:20:55 +0100

    Update run.sh

--- monster-ui/run.sh
+++ monster-ui/run.sh
@@ -2,8 +2,6 @@
 FLAGS=${1:-"-td"}
 NAME=${NAME:-"monster-ui.kazoo"}
 
-#read -p "Enter Kazoo Server IP:" KAZOO
-
 docker ps -aq -f name=$NAME | while read ID; do
         echo -n "stopping: "
         docker stop -t 1 $ID
