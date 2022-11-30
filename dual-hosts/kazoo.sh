91855ccdaaefd8d4cfa5a7aa2a2887f769efb239
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-30 09:01:51 +0100

    Update kazoo.sh

--- dual-hosts/kazoo.sh
+++ dual-hosts/kazoo.sh
@@ -5,14 +5,6 @@
 ../kamailio/run.sh
 ../monster-ui/run.sh
 
-for CONTAINER in freeswitch kazoo
-do
-	if [ "$(docker inspect -f {{.State.Running}} $CONTAINER.kazoo)" = "true" ]
-	then
-		echo -n '$KAZOOIP' $CONTAINER.$NETWORK
-	fi
-done
-
 ../hosts.sh >> /etc/hosts
 
 exit
