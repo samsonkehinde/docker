323b3f4d677816817f2c2018a9a8107e42c5f74d
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-30 09:22:51 +0100

    Update hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -1,10 +1,28 @@
 #!/bin/sh -e
-NETWORK=${1:-"kazoo"}
-for CONTAINER in monster-ui kazoo rabbitmq freeswitch kamailio couchdb #makebusy makebusy-fs-auth makebusy-fs-pbx makebusy-fs-carrier
-do
-	if [ "$(docker inspect -f {{.State.Running}} $CONTAINER.$NETWORK)" = "true" ]
-	then
-		echo `bin/get-ip $CONTAINER.$NETWORK` $CONTAINER.$NETWORK
-	fi
-done
-exit
+
+ZONE_IP=${ZONE_IP:-""}
+KAZOO_IP=${ZONE_IP:-""}
+
+if [ -n ZONE_IP ]; then
+	for CONTAINER in freeswitch kazoo; do
+		if [ "$(docker inspect -f {{.State.Running}} $CONTAINER.kazoo)" = "true" ]; then
+			echo -n '$ZONE_IP $CONTAINER.kazoo\n'
+		fi
+	done
+	
+	for CONTAINER in monster-ui rabbitmq kamailio couchdb; do
+		echo -n '127.0.0.1 $CONTAINER.kazoo\n'
+	done
+fi
+
+if [ -n KAZOO_IP ]; then
+	for CONTAINER in monster-ui rabbitmq kamailio couchdb; do
+		if [ "$(docker inspect -f {{.State.Running}} $CONTAINER.kazoo)" = "true" ]; then
+			echo -n '$KAZOO_IP $CONTAINER.kazoo\n'
+		fi
+	done
+	
+	for CONTAINER in freeswitch kazoo; do
+		echo -n '127.0.0.1 $CONTAINER.kazoo\n'
+	done
+fi
