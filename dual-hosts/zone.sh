2363a803658883b0f5c76802c88b71eb164f7607
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-30 09:01:11 +0100

    Update zone.sh

--- dual-hosts/zone.sh
+++ dual-hosts/zone.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 
-read -p "Enter the IP of your RABBITMQ server: " RABBITMQ
-read -p "Enter the IP of your COUCHDB server: " COUCHDB
-
-export RABBITMQ=$RABBITMQ
-export COUCHDB=$COUCHDB
+export RABBITMQ=${RABBITMQ:-"rabbitmq.kazoo"}
+export COUCHDB=${COUCHDB:-"rabbitmq.couchdb"}
 
 ../kazoo/run.sh
 ../freeswitch/run.sh
 
-for CONTAINER in monster-ui rabbitmq kamailio couchdb
-do
-	if [ "$(docker inspect -f {{.State.Running}} $CONTAINER.kazoo)" = "true" ]
-	then
-		echo $RABBITMQ $CONTAINER.$NETWORK
-	fi
-done
+../hosts.sh >> /etc/hosts
+
 exit
-
-
