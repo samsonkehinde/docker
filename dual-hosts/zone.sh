b4725ee3a13127943365f281b1303863d66bc3ee
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-30 11:00:38 +0100

    Update zone.sh

--- dual-hosts/zone.sh
+++ dual-hosts/zone.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 read -p "Enter the IP of KAZOO HOST: " KAZOO_IP
-export KAZOO_IP
+RABBITMQ=KAZOO_IP
+COUCHDB=KAZOO_IP
+
+export RABBITMQ COUCHDB
 
 ../kazoo/run.sh
 ../freeswitch/run.sh
 
-../hosts.sh >> /etc/hosts
+#../hosts.sh >> /etc/hosts
 
 exit
