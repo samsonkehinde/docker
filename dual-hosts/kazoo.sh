937668b615a882e30b136b98aba35abb032d286e
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-30 11:39:29 +0100

    Update kazoo.sh

--- dual-hosts/kazoo.sh
+++ dual-hosts/kazoo.sh
@@ -3,11 +3,11 @@
 read -p "Enter the IP of ZONE HOST: " ZONE_IP
 export ZONE_IP
 
-../rabbitmq/run.sh
-../couchdb/run.sh
-../kamailio/run.sh
-../monster-ui/run.sh
+FLAGS=`../hosts.sh`
 
-../hosts.sh >> /etc/hosts
+FLAGS=$FLAGS ../rabbitmq/run.sh
+FLAGS=$FLAGS ../couchdb/run.sh
+FLAGS=$FLAGS ../kamailio/run.sh
+FLAGS=$FLAGS ../monster-ui/run.sh
 
 exit
