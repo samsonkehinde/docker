313de9a8ad22b871a7c9aa2ae6950d34a10c41c0
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-30 11:31:44 +0100

    Update zone.sh

--- dual-hosts/zone.sh
+++ dual-hosts/zone.sh
@@ -4,9 +4,10 @@ read -p "Enter the IP of KAZOO HOST: " KAZOO_IP
 
 export KAZOO_IP
 
+FLAGS = `../hosts.sh`
+export $FLAGS
+
 ../kazoo/run.sh
 ../freeswitch/run.sh
-
-#../hosts.sh >> /etc/hosts
 
 exit
