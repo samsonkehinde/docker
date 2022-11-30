0672c644273a481d8d8dfdb70c218eaeb42722b6
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-11-30 09:48:09 +0100

    Update hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -6,23 +6,23 @@ KAZOO_IP=${KAZOO_IP:-""}
 if [ -n ZONE_IP ]; then
 	for CONTAINER in freeswitch kazoo; do
 		if [ "$(docker inspect -f {{.State.Running}} $CONTAINER.kazoo)" = "true" ]; then
-			echo -n '$ZONE_IP $CONTAINER.kazoo\n'
+			echo "$ZONE_IP $CONTAINER.kazoo"
 		fi
 	done
 	
 	for CONTAINER in monster-ui rabbitmq kamailio couchdb; do
-		echo -n '127.0.0.1 $CONTAINER.kazoo\n'
+		echo "127.0.0.1 $CONTAINER.kazoo"
 	done
 fi
 
 if [ -n KAZOO_IP ]; then
 	for CONTAINER in monster-ui rabbitmq kamailio couchdb; do
 		if [ "$(docker inspect -f {{.State.Running}} $CONTAINER.kazoo)" = "true" ]; then
-			echo -n '$KAZOO_IP $CONTAINER.kazoo\n'
+			echo "$KAZOO_IP $CONTAINER.kazoo"
 		fi
 	done
 	
 	for CONTAINER in freeswitch kazoo; do
-		echo -n '127.0.0.1 $CONTAINER.kazoo\n'
+		echo "127.0.0.1 $CONTAINER.kazoo"
 	done
 fi
