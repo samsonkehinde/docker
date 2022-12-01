d117f085fa34916c75ffdd03897f84a7d5219df5
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2022-12-01 08:45:11 +0100

    Update download_zone.sh

--- dual-hosts/download_zone.sh
+++ dual-hosts/download_zone.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+docker rmi -f $(docker images | grep "kehindesamson/kazoo" | cut -d' ' -f 1)
+
+docker pull kehindesamson/kazoo:kazoo
+docker tag kehindesamson/kazoo:kazoo kazoo/kazoo
+
+docker pull kehindesamson/kazoo:freeswitch
+docker tag kehindesamson/kazoo:freeswitch kazoo/freeswitch
 
