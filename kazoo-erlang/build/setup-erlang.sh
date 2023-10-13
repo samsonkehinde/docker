534b6f4f2f3c2e14a30916152eefdd4bdf4eb286
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2023-10-13 23:35:15 +0100

    Add Kazoo to build

--- kazoo-erlang/build/setup-erlang.sh
+++ kazoo-erlang/build/setup-erlang.sh
@@ -1,9 +1,20 @@
 #!/bin/sh -e
-curl -O https://raw.githubusercontent.com/kerl/kerl/master/kerl
-chmod +x kerl
-mv kerl /usr/bin
-kerl update releases
-kerl build 19.3.6.5 19.3.6.5 # this takes a while
-kerl install 19.3.6.5 erlang
-. erlang/activate
-kerl cleanup all
+
+git clone https://github.com/erlang/otp.git
+cd otp
+export ERL_TOP=`pwd`
+
+git checkout maint-19
+./configure
+make
+make install
+export PATH=$ERL_TOP/bin:$PATH  
+
+# curl -O https://raw.githubusercontent.com/kerl/kerl/master/kerl
+# chmod +x kerl
+# mv kerl /usr/bin
+# kerl update releases
+# kerl build 19.3.6.5 19.3.6.5 # this takes a while
+# kerl install 19.3.6.5 erlang
+# . erlang/activate
+# kerl cleanup all
--- kazoo/build/setup.sh
+++ kazoo/build/setup.sh
@@ -1,4 +1,11 @@
 #!/bin/sh -e
+apt-get update
+apt-get upgrade -y
+
+apt-get -y --force-yes install vim curl wget git procps make python python3 build-essential zip unzip libexpat-dev unixodbc-dev libssh2-1-dev libssl-dev libncurses5-dev autoconf
+
+apt-get -y clean
+
 if [ -n "$SKIP_BUILD" ]
 then
 	touch skip_build
@@ -8,7 +15,7 @@ fi
 # REPO is global and must be defined on build
 git clone $REPO kazoo
 
-. /home/user/erlang/activate
+#. /home/user/erlang/activate
 
 COMMIT=$(cat commit)
 
