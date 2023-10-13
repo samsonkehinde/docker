fcedb2179b7fc9efb2c85fba19a41d49c328370f
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2023-10-13 20:53:48 +0100

    Create run.sh

---
+++ base-os/couchdb/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+FLAGS=${1:-"-td"}
+NETWORK=${NETWORK:-"kazoo"}
+IMAGE=${2:-"$NETWORK/couchdb"}
+NAME=couchdb.$NETWORK
+
+if [ -n "$(docker ps -aq -f name=$NAME)" ]
+then
+   echo -n "stopping: "
+   docker stop -t 1 $NAME
+   echo -n "removing: "
+   docker rm -f $NAME
+fi
+echo -n "starting: $NAME "
+
+#docker run $FLAGS --net $NETWORK -h $NAME --name $NAME $IMAGE
+docker run $FLAGS --net $NETWORK -h $NAME -p 8081:5984 -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=admin \
+--name $NAME $IMAGE
