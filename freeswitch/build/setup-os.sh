50644e958112d9e974c86a3bb5bb43cb5246d20f
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2023-10-14 21:14:37 +0100

    freeswitch install

--- freeswitch/build/setup-os.sh
+++ freeswitch/build/setup-os.sh
@@ -1,19 +1,17 @@
 #!/bin/sh -e
 #read -p 'Enter Freeswitch Token' TOKEN
 
-apt-get update
-apt-get install -y gnupg2 wget lsb-release apt-transport-https ca-certificates libldns-dev
-
+apt-get update && apt-get install -yq gnupg2 wget lsb-release
 wget --http-user=signalwire --http-password=$TOKEN -O /usr/share/keyrings/signalwire-freeswitch-repo.gpg https://freeswitch.signalwire.com/repo/deb/debian-release/signalwire-freeswitch-repo.gpg
 
 echo "machine freeswitch.signalwire.com login signalwire password $TOKEN" > /etc/apt/auth.conf
+chmod 600 /etc/apt/auth.conf
 echo "deb [signed-by=/usr/share/keyrings/signalwire-freeswitch-repo.gpg] https://freeswitch.signalwire.com/repo/deb/debian-release/ `lsb_release -sc` main" > /etc/apt/sources.list.d/freeswitch.list
 echo "deb-src [signed-by=/usr/share/keyrings/signalwire-freeswitch-repo.gpg] https://freeswitch.signalwire.com/repo/deb/debian-release/ `lsb_release -sc` main" >> /etc/apt/sources.list.d/freeswitch.list
 
 apt-get update
-
+ 
 # Install dependencies required for the build
-apt-get -y build-dep freeswitch
-#apt-get install -y libg7221-dev
+apt-get build-dep freeswitch
 
 apt-get -y clean
