e154a97f72eab56a93824e30839ded3ad2150e46
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2023-10-14 17:00:44 +0100

    git push

--- kamailio/build/setup-os.sh
+++ kamailio/build/setup-os.sh
@@ -1,6 +1,15 @@
 #!/bin/sh -e
+# apt-get -y install \
+# 	make flex bison libmysqlclient-dev \
+# 	libssl-dev libcurl4-openssl-dev \
+# 	libxml2-dev libpcre3-dev librabbitmq-dev \
+# 	libjson-c-dev pkg-config  libevent-dev libev-dev \
+# 	uuid-dev libunistring-dev xsltproc \
+# 	python libsnmp-dev bison
 
-apt-get install -y make python build-essential zip unzip libexpat-dev \
+#!/bin/sh -e
+apt-get install -y make libmysqlclient-dev libsnmp-dev bison python build-essential zip unzip libexpat-dev \
 	unixodbc-dev libssh2-1-dev libssl-dev libncurses5-dev python
-	
+apt-get -y clean
+
 apt-get -y clean
