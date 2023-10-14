167f5ecd9bf5e22cc6c4df876d8422070ff3b814
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2023-10-14 21:52:25 +0100

    freeswitch install

--- freeswitch/build/configure.sh
+++ freeswitch/build/configure.sh
@@ -6,7 +6,7 @@ cd freeswitch
 sed -i "s/^kazoo_globals.legacy_events = SWITCH_FALSE;/kazoo_globals.legacy_events = SWITCH_TRUE;/g" src/mod/event_handlers/mod_kazoo/kazoo_ei_config.c
 
 # Enable the debugging of the messages sent between kazoo and freeswitch
-sed -i '1,10s/^/#define EI_DEBUG/' src/mod/event_handlers/mod_kazoo/mod_kazoo.h
+sed -i '1,10s/^/#define EI_DEBUG\r\n/' src/mod/event_handlers/mod_kazoo/mod_kazoo.h
 
 ./bootstrap.sh -j
 ./configure
