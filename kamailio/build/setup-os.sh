5014e092e0eea2edf957822fd7a3c85103faa680
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2023-10-14 19:33:56 +0100

    kamailio fix

--- kamailio/build/setup-os.sh
+++ kamailio/build/setup-os.sh
@@ -7,7 +7,7 @@
 # 	uuid-dev libunistring-dev xsltproc \
 # 	python libsnmp-dev bison
 
-apt-get install -y make flex libsnmp-dev bison python build-essential zip unzip libexpat-dev \
-	unixodbc-dev libssh2-1-dev libssl-dev libncurses5-dev libjson-c-dev libjsoncpp-dev
+apt-get install -y make flex libsnmp-dev bison python build-essential zip unzip libexpat-dev librabbitmq-dev libpcre3-dev libxml2-dev\
+	unixodbc-dev libssh2-1-dev libssl-dev libncurses5-dev libjson-c-dev pkg-config  libunistring-dev uuid-dev  libevent-dev libev-dev libcurl4-openssl-dev
 
 apt-get -y clean
