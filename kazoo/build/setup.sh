937ab4dc9b385290f0e977c2c0d1946808b76ab1
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2023-10-15 21:55:31 +0100

    kazoo debug

--- kazoo/build/setup.sh
+++ kazoo/build/setup.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get upgrade -y
 
-apt-get -y --force-yes install erlang-dev vim curl wget git procps make python python3 build-essential zip unzip libexpat-dev unixodbc-dev libssh2-1-dev libssl-dev libncurses5-dev autoconf
+apt-get -y --force-yes install erlang-dev vim curl wget git procps make python python3 build-essential zip unzip libexpat-dev unixodbc-dev libssh-4 libssh-dev libncurses5-dev autoconf
 
 apt-get -y clean
 
