433f75e58dad59c607684cb28c8a4106af174e2a
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2023-10-17 21:34:02 +0100

    kamailio legacy build kit build

--- kamailio/build.sh
+++ kamailio/build.sh
@@ -5,4 +5,4 @@ COMMIT=${1:-"$(../bin/get-commit $REPO)"}
 
 echo $COMMIT > etc/commit
 
-DOCKER_BUILDKIT=0 docker build $BUILD_FLAGS --build-arg REPO=$REPO --build-arg CONFIG=63cece75a767b70f385a481b22bd6e5cec384ae5 -t $NETWORK/kamailio .
+DOCKER_BUILDKIT=0 docker build $BUILD_FLAGS --build-arg REPO=$REPO --build-arg CONFIG=8afbef27ddb4d0ceb325a993c9f349da7ea5e82a -t $NETWORK/kamailio .
