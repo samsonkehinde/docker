07dedb079caf2e1f443ce7f79b8dbaa5dbe0e574
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2023-10-23 09:43:50 +0100

    install freeswitch as root

--- freeswitch/build/install.sh
+++ freeswitch/build/install.sh
@@ -2,14 +2,14 @@
 cd freeswitch
 make install
 
-groupadd freeswitch
+#groupadd freeswitch
 
-adduser --disabled-password  --quiet --system --home /usr/local/freeswitch --ingroup freeswitch freeswitch
+#adduser --disabled-password  --quiet --system --home /usr/local/freeswitch --ingroup freeswitch freeswitch
 
 git clone --depth 1 --no-single-branch https://github.com/2600hz/kazoo-sounds
 cp -a kazoo-sounds/freeswitch /usr/local/freeswitch/sounds
 
-chown -R freeswitch:freeswitch /usr/local/freeswitch/
+#chown -R freeswitch:freeswitch /usr/local/freeswitch/
 chmod -R ug=rwX,o= /usr/local/freeswitch/
 chmod -R u=rwx,g=rx /usr/local/freeswitch/bin/*
 
