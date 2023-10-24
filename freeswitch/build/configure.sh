28b3fb7f84d9e6101e1a1db7f801673f31d4d3a2
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2023-10-24 05:08:38 +0100

    freeswitch - corrected enabling of legacy events

--- freeswitch/build/configure.sh
+++ freeswitch/build/configure.sh
@@ -3,7 +3,7 @@
 cd freeswitch
 
 # Enable mod_kazoo legacy events 
-sed -i "s/^kazoo_globals.legacy_events = SWITCH_FALSE;/kazoo_globals.legacy_events = SWITCH_TRUE;/g" src/mod/event_handlers/mod_kazoo/kazoo_ei_config.c
+sed -i "s/kazoo_globals.legacy_events = SWITCH_FALSE;/kazoo_globals.legacy_events = SWITCH_TRUE;/g" src/mod/event_handlers/mod_kazoo/kazoo_ei_config.c
 
 # Enable the debugging of the messages sent between kazoo and freeswitch
 sed -i '1,10s/^/#define EI_DEBUG\r\n/' src/mod/event_handlers/mod_kazoo/mod_kazoo.h
