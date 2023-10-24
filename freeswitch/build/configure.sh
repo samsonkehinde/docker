b604891fb751f6a502287359b867d3c4c299a71e
Samson Kehinde <41105905+samsonkehinde@users.noreply.github.com>
2023-10-24 06:59:17 +0100

    disabled debugging of erland distribution protocol

--- freeswitch/build/configure.sh
+++ freeswitch/build/configure.sh
@@ -3,10 +3,10 @@
 cd freeswitch
 
 # Enable mod_kazoo legacy events 
-sed -i "s/kazoo_globals.legacy_events = SWITCH_FALSE;/kazoo_globals.legacy_events = SWITCH_TRUE;/g" src/mod/event_handlers/mod_kazoo/kazoo_ei_config.c
+#sed -i "s/kazoo_globals.legacy_events = SWITCH_FALSE;/kazoo_globals.legacy_events = SWITCH_TRUE;/g" src/mod/event_handlers/mod_kazoo/kazoo_ei_config.c
 
 # Enable the debugging of the messages sent between kazoo and freeswitch
-sed -i '1,10s/^/#define EI_DEBUG\r\n/' src/mod/event_handlers/mod_kazoo/mod_kazoo.h
+#sed -i '1,10s/^/#define EI_DEBUG\r\n/' src/mod/event_handlers/mod_kazoo/mod_kazoo.h
 
 ./bootstrap.sh -j
 ./configure
